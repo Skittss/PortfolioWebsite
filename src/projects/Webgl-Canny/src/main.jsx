60ce783076eaf3954d0aa715ea2b55a295815407
Skitts <hennoallen@hotmail.co.uk>
2021-06-04 04:08:36 +0100

    Updated Canny Home Page

--- src/projects/Webgl-Canny/src/main.jsx
+++ src/projects/Webgl-Canny/src/main.jsx
@@ -13,7 +13,7 @@ const Main = () => {
         if (notifSent === false) {
             notification.open({
                 message: "This project works best on a powerful machine!",
-                description: <span>This project makes use of WebGL - a dedicated GPU is STRONGLY recommended.<br />Loading images which are too large or updating parameters too quickly <i>may</i> cause the browser to run out of memory.</span>,
+                description: <span>This page uses WebGL - a dedicated GPU is STRONGLY recommended.<br />Loading images which are too large or updating parameters too quickly <i>may</i> cause the browser to run out of memory.</span>,
                 icon: <WarningTwoTone twoToneColor="#FFA500" />,
                 key: "performance-notif",
                 style: {width: 500}
