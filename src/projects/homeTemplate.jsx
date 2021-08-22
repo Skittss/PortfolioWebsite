e70bdc439765fd0a598d77647c4dbcfc0fbe5c4e
Skitts <hennoallen@hotmail.co.uk>
2021-08-22 01:32:06 +0100

    Added top anchor

--- src/projects/homeTemplate.jsx
+++ src/projects/homeTemplate.jsx
@@ -3,11 +3,12 @@ import { Link, useLocation } from 'react-router-dom';
 import { Divider, Tooltip } from 'antd';
 import { GithubOutlined, SendOutlined } from '@ant-design/icons';
 
-const HomeTemplate = ({title, githubURL, projectRoute}) => {
+const HomeTemplate = ({title, githubURL, projectRoute, projectLink}) => {
     const loc = useLocation().pathname;
     return (
         <div className="project-home-wrapper">
             <header className="home-header">
+                <a name="top" />
                 <h1 id="title" style={{display: 'inline-block'}}>{title}</h1>
                 <span style={{padding: "0 1em", display: 'inline-block'}}>
                     {githubURL != undefined ? (
@@ -25,6 +26,15 @@ const HomeTemplate = ({title, githubURL, projectRoute}) => {
                             </Link>
                         </Tooltip>
                     ) : null}
+
+                    {projectLink != undefined  ? (
+                        <Tooltip title="View project" placement="bottom">
+                            <a href={projectLink} target="_blank">
+                                <SendOutlined style={{fontSize: "40px", padding: "0 0.2em"}}/>
+                            </a>
+                        </Tooltip>
+                    ) : null}
+
                 </span>
                 <Divider style={{borderTopWidth: "1px", borderTopColor: "#000000", opacity: 0.5}}/>
             </header>
