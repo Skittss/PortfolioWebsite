06b7ae265e3b39c05c9c7996a55006fdee86b59e
Skitts <hennoallen@hotmail.co.uk>
2021-08-22 01:33:13 +0100

    Added fourier sketcher project

---
+++ src/projects/FourierSketcher/index.jsx
@@ -0,0 +1,13 @@
+import img from "../../content/projects/FourierSketcher/thumb.png";
+import router from "./router";
+
+const Meta = {
+    title: "Fourier Sketcher (Prototype)",
+    thumb: img,
+    tags: ["JavaScript", "~~p5js~~"],
+    legacy: false,
+    route: "/FourierSketcher",
+    router: router
+}
+
+export default Meta;
