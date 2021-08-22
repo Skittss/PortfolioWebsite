3cf988a270a40c18ca4cae73f8ba1b704a962d89
Skitts <hennoallen@hotmail.co.uk>
2021-08-22 01:34:22 +0100

    Code quality change

--- src/projects/Website/index.jsx
+++ src/projects/Website/index.jsx
@@ -1,9 +1,10 @@
-import img from "../../content/projects/FourierSketcher/img/mononoke.jpg"
+import img from "../../content/projects/Website/thumb.png"
 
 const Meta = {
     title: "This Website!",
     thumb: img,
     tags: ["ReactJS", "~~antd~~"],
+    legacy: false,
     route: null,
     tooltip: "This page is part of this project!"
 }
